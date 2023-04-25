38ba522876a8fe31f36cb72c0e39fc1ee276c33c
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-04-25 20:06:58 +0200

    style: add font variables

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,33 @@
 import type { AppProps } from "next/app";
+import { Manrope, Poppins } from "next/font/google";
 import Head from "next/head";
 
 import PrimaryLayout from "../components/layout/primary/PrimaryLayout";
 import "../styles/globals.scss";
 
+const manrope = Manrope({ weight: ["400", "700"], subsets: ["latin"] });
+const poppins = Poppins({ weight: ["400", "700"], subsets: ["latin"] });
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PrimaryLayout>
-      <Head>
-        <title>Syndicate Crime</title>
-        <meta name="description" content="Syndicate Crime game" />
-        <meta name="viewport" content="initial-scale=1.0, width=device=width" />
-      </Head>
-      <Component {...pageProps} />
-    </PrimaryLayout>
+    <>
+      <style jsx global>{`
+        :root {
+          --manrope-font: ${manrope.style.fontFamily};
+          --poppins-font: ${poppins.style.fontFamily};
+        }
+      `}</style>
+      <PrimaryLayout>
+        <Head>
+          <title>Syndicate Crime</title>
+          <meta name="description" content="Syndicate Crime game" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device=width"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </PrimaryLayout>
+    </>
   );
 }
