fe0600b9dc02d9955e7804c1533e81e274e2354e
Piotr Rzadkowolski <p.rzadkowolski@gmail.com>
2023-04-29 21:01:22 +0200

    style: add UiKit component and page

---
+++ pages/uikit/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+import UiKit from "@/components/layout/uikit/Uikit";
+
+export default function Uikit() {
+  return (
+    <>
+      <Head>
+        <title>Syndicate Crime Uikit</title>
+      </Head>
+      <main>
+        <UiKit />
+      </main>
+    </>
+  );
+}
