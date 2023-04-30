09176bcc2e52dc65ce6b68d81fe758022729bb07
Piotr Rzadkowolski <p.rzadkowolski@gmail.com>
2023-04-30 09:16:14 +0200

    style: add color variables to UiKit

--- components/layout/uikit/Uikit.tsx
+++ components/layout/uikit/Uikit.tsx
@@ -3,11 +3,13 @@ import styles from "./Uikit.module.scss";
 const UiKit: React.FC = () => {
   return (
     <div className={styles.container}>
-      <h1>Color Variables</h1>
-      <p>$dark-gray: #333333</p>
-      <p>$dark-red: #990000</p>
-      <p>$base-gold: #ffd700</p>
-      <p>$base-orange: #ffa500</p>
+      <div className={styles.colors}>
+        <h1>Color Variables</h1>
+        <p>$dark-gray: #333333</p>
+        <p>$dark-red: #990000</p>
+        <p>$base-gold: #ffd700</p>
+        <p>$base-orange: #ffa500</p>
+      </div>
     </div>
   );
 };
