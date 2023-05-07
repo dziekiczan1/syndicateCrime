7816ec9bcb4e06246b25aab35dcddc26adff9a01
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-05-07 16:15:50 +0200

    feat: create Avatar component

---
+++ components/user/avatar/Avatar.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+import styles from "./Avatar.module.scss";
+
+export interface IAvatarProps {
+  src: string;
+  width: number;
+  height: number;
+  alt: string;
+}
+
+const Avatar: React.FC<IAvatarProps> = ({ src, width, height, alt }) => {
+  return (
+    <Image
+      src={src}
+      width={width}
+      height={height}
+      alt={alt}
+      className={styles.avatar}
+    />
+  );
+};
+
+export default Avatar;
