d158837fbf8a5b2d9fac6fdf78e3861792dacc30
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-05-07 16:09:17 +0200

    feat: add first part of avatars

---
+++ constants/avatars.tsx
@@ -0,0 +1,2 @@
+const avatarItems = [{ src: "av1" }, { src: "av2" }];
+export default avatarItems;
--- constants/images.tsx
+++ constants/images.tsx
@@ -3,10 +3,26 @@ import { StaticImageData } from "next/image";
 import loginBackground from "../public/assets/background/login.webp";
 import logoDesktop from "../public/assets/logo/desktop.webp";
 
+import avatar1 from "../public/assets/avatars/av1.webp";
+import avatar2 from "../public/assets/avatars/av2.webp";
+import avatar3 from "../public/assets/avatars/av3.webp";
+import avatar4 from "../public/assets/avatars/av4.webp";
+import avatar5 from "../public/assets/avatars/av5.webp";
+import avatar6 from "../public/assets/avatars/av6.webp";
+
 export interface IImages {
   [key: string]: StaticImageData;
 }
 
-const images: IImages = { loginBackground, logoDesktop };
+const images: IImages = {
+  loginBackground,
+  logoDesktop,
+  avatar1,
+  avatar2,
+  avatar3,
+  avatar4,
+  avatar5,
+  avatar6,
+};
 
 export default images;
