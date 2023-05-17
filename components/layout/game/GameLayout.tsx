20f0d55a69d245eb7de287290e92008f55f483e6
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-05-17 19:08:39 +0200

    refactor: move part of code to GameLayout component

--- components/layout/game/GameLayout.tsx
+++ components/layout/game/GameLayout.tsx
@@ -1,5 +1,7 @@
+import ActionsInterface from "@/components/user/actions/ActionsInterface";
 import UserInterface from "@/components/user/interface/UserInterface";
 import { ReactNode } from "react";
+import Logo from "../logo/Logo";
 
 export interface IGameLayout {
   children?: ReactNode;
@@ -7,12 +9,16 @@ export interface IGameLayout {
 
 const GameLayout: React.FC<IGameLayout> = ({ children }) => {
   return (
-    <div className="no-scrollbar relative z-1 overflow-y-scroll">
+    <div className="relative z-1">
       <div className="flex p-8">
-        <div className="flex w-1/2">
+        <div className="flex flex-col gap-8 w-1/2">
           <UserInterface />
+          <ActionsInterface />
         </div>
-        <div className="flex justify-center w-1/2">{children}</div>
+        <div className="flex justify-center w-1/2 h-fit">
+          <Logo width={300} height={180} />
+          {children}
+        </div>
       </div>
     </div>
   );
--- pages/game/index.tsx
+++ pages/game/index.tsx
@@ -5,11 +5,11 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import { w3cwebsocket } from "websocket";
 
-import Logo from "@/components/layout/logo/Logo";
-import ActionsInterface from "@/components/user/actions/ActionsInterface";
-import UserInterface from "@/components/user/interface/UserInterface";
+import GameLayout from "@/components/layout/game/GameLayout";
+import { images } from "@/constants";
 import { connectToDatabase } from "@/lib/db";
 import UserContext, { IUser } from "@/store/user-context";
+import Image from "next/image";
 
 export interface IGameMainScreenProps {
   session: Session;
@@ -39,27 +39,17 @@ export default function GameMainScreen(props: IGameMainScreenProps) {
         <title>Syndicate Crime</title>
       </Head>
       <div className="h-screen no-scrollbar overflow-y-scroll">
-        <div className="absolute -z-10 h-screen w-screen">
-          {/* <Image
+        <div className="absolute -z-10 h-screen w-screen grayscale opacity-25">
+          <Image
             src={images.loginBackground}
             alt="Login Background"
             width={0}
             height={0}
             sizes="100vw"
             className="object-cover w-full h-full"
-          /> */}
+          />
         </div>
-        <div className="relative z-1">
-          <div className="flex p-8">
-            <div className="flex flex-col gap-8 w-1/2">
-              <UserInterface />
-              <ActionsInterface />
-            </div>
-            <div className="flex justify-center w-1/2 h-fit">
-              <Logo width={300} height={180} />
-            </div>
-          </div>
-        </div>
+        <GameLayout />
       </div>
     </UserContext.Provider>
   );
