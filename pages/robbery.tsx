74f67c1d438d86510c101c54a9f213ed5f2f2dff
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-05-19 17:45:59 +0200

    refactor: remove robbery page

--- pages/robbery.tsx
+++
@@ -1,8 +0,0 @@
-import GameLayout from "@/components/layout/game/GameLayout";
-import UserContext from "@/store/user-context";
-import { useContext } from "react";
-
-export default function Robbery() {
-  const { user } = useContext(UserContext);
-  return <GameLayout>:=)</GameLayout>;
-}
