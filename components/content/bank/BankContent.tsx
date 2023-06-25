3170021b6bfaa180c1bba8d37aebdfa7274b890a
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-06-25 15:36:27 +0200

    style: add styles for BankContent component

--- components/content/bank/BankContent.tsx
+++ components/content/bank/BankContent.tsx
@@ -2,18 +2,21 @@ import InputField from "@/components/auth/InputField";
 import Button from "@/components/ui/button/Button";
 import PageHeader from "@/components/ui/pageheader/PageHeader";
 import pageDescriptions from "@/constants/pagedescriptions";
-import { useState } from "react";
+import UserContext from "@/store/user-context";
+import { useContext, useState } from "react";
 import { FieldError, useForm } from "react-hook-form";
 import styles from "./BankContent.module.scss";
 
 const BankContent: React.FC = () => {
   const { title, description } = pageDescriptions.bank;
+  const { user, setUser } = useContext(UserContext);
+  const [isStash, setIsStash] = useState(true);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const [isStash, setIsStash] = useState(true);
+  const userSavings = user && user.bank;
 
   const validationRules = {
     bankmoney: {
@@ -51,12 +54,12 @@ const BankContent: React.FC = () => {
         },
       });
 
-      if (response.ok) {
+      if (setUser && response.ok) {
         const updatedUser = await response.json();
-        console.log(updatedUser);
+        setUser(updatedUser);
       }
     } catch (error) {
-      console.error("There was a problem.", error);
+      console.error("Error processing bank action.", error);
     }
     return null;
   };
@@ -64,6 +67,15 @@ const BankContent: React.FC = () => {
   return (
     <div className={styles.container}>
       <PageHeader title={title} description={description} />
+      <div className={styles.savings}>
+        {userSavings ? (
+          <h4>
+            Your Current Savings: <span>${userSavings.toLocaleString()}</span>
+          </h4>
+        ) : (
+          <h4>You don&apos;t have any funds in your bank account</h4>
+        )}
+      </div>
       <div className={styles.control}>
         <form onSubmit={handleSubmit(onSubmit)}>
           <p className={errors.bankmoney && styles.message}>
@@ -81,7 +93,7 @@ const BankContent: React.FC = () => {
           />
           <div className={styles.actions}>
             <Button onClick={switchBankActionHandler}>
-              {isStash ? "I want to stash" : "I want to withdraw"}
+              {isStash ? "I want to withdraw" : "I want to stash"}
             </Button>
             <Button form={true} secondary>
               {isStash ? "Stash your money!" : "Withdraw your money!"}
