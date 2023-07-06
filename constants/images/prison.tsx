bccdc265a9244618a9d339541ddd93387fea1516
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-07-06 20:05:04 +0200

    feat: add freedom picture for prison, reduce images size

--- constants/images/prison.tsx
+++ constants/images/prison.tsx
@@ -2,10 +2,12 @@ import { IImages } from "../images";
 
 import bailout from "../../public/assets/prison/bailout.webp";
 import escape from "../../public/assets/prison/escape.webp";
+import freedom from "../../public/assets/prison/freedom.webp";
 
 const prisonImages: IImages = {
   bailout,
   escape,
+  freedom,
 };
 
 export default prisonImages;
