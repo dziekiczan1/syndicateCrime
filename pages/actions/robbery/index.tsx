04321daa2525b192730701127af419c09b605654
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-07-17 14:33:23 +0200

    feat: add with prison check for robbery page

--- pages/actions/robbery/index.tsx
+++ pages/actions/robbery/index.tsx
@@ -1,7 +1,7 @@
 import RobberyContent from "@/components/content/robbery/RobberyContent";
 import GameLayout from "@/components/layout/game/GameLayout";
-import { GetServerSidePropsContext } from "next";
-import { getSession } from "next-auth/react";
+import { withPrisonCheck } from "@/lib/withPrisonCheck";
+import { withSessionCheck } from "@/lib/withSessionCheck";
 
 export default function GameMainScreen() {
   return (
@@ -13,19 +13,10 @@ export default function GameMainScreen() {
   );
 }
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const session = await getSession(context);
-
-  if (!session) {
+export const getServerSideProps = withPrisonCheck(
+  withSessionCheck(async () => {
     return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
+      props: {},
     };
-  }
-
-  return {
-    props: {},
-  };
-}
+  })
+);
