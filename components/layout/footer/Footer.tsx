6ccbea60bc57cd587bbf7d98b05d4fd58c0cf0c8
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-07-18 21:08:48 +0200

    style: add basic styles for Footer component

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -1,7 +1,24 @@
 import styles from "./Footer.module.scss";
 
 const Footer: React.FC = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.sections}>
+        <ul className={styles.section}>
+          <h4 className={styles.sectionTitle}>Title</h4>
+          <li>Some Link</li>
+        </ul>
+        <ul className={styles.section}>
+          <h4 className={styles.sectionTitle}>Title</h4>
+          <li>Some Link</li>
+        </ul>
+        <ul className={styles.section}>
+          <h4 className={styles.sectionTitle}>Title</h4>
+          <li>Some Link</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
