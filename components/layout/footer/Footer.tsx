faf3bd4ba3a03491d4fe307b1aac129446d12e12
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-07-19 18:26:41 +0200

    style: update styles for Footer component

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -1,21 +1,58 @@
+import Link from "next/link";
 import styles from "./Footer.module.scss";
 
 const Footer: React.FC = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.sections}>
-        <ul className={styles.section}>
-          <h4 className={styles.sectionTitle}>Title</h4>
-          <li>Some Link</li>
-        </ul>
-        <ul className={styles.section}>
-          <h4 className={styles.sectionTitle}>Title</h4>
-          <li>Some Link</li>
-        </ul>
-        <ul className={styles.section}>
-          <h4 className={styles.sectionTitle}>Title</h4>
-          <li>Some Link</li>
-        </ul>
+      <div className={styles.sectionsWrapper}>
+        <div className={styles.menuSections}>
+          <ul className={styles.section}>
+            <h3 className={styles.sectionTitle}>Menu</h3>
+            <li>
+              <Link href="/">
+                <p>Home Page</p>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <p>Profile</p>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <p>Statistics</p>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <p>Credit</p>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <p>Help</p>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <p>Logout</p>
+              </Link>
+            </li>
+          </ul>
+          <ul className={styles.section}>
+            <h3 className={styles.sectionTitle}>Contact us!</h3>
+            <li>
+              <p>Some Link</p>
+            </li>
+          </ul>
+          <ul className={styles.section}>
+            <h3 className={styles.sectionTitle}>Social Media</h3>
+            <li>
+              <p>Some Link</p>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.imageSection}></div>
       </div>
     </div>
   );
