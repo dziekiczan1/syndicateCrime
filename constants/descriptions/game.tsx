ae641c04e835dc6b50baf77336cdc8c7d6139f0b
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-08-09 20:31:38 +0200

    test: develop deploy test 4

--- constants/descriptions/game.tsx
+++ constants/descriptions/game.tsx
@@ -2,7 +2,7 @@ import sectionsImages from "../images/sections";
 
 const mainNews = [
   {
-    title: "The Crime News Develop 3",
+    title: "The Crime News Develop 4",
     date: "Published on May 24, 2023",
     subtitle: "Introducing Syndicate Crime: Dive into the Dark World of Mafia",
     paragraphs: [
