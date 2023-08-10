df7174258d99db10fc58d0b0f9484dc5d5957445
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-08-10 20:43:57 +0200

    feat: add university images

---
+++ constants/images/university.tsx
@@ -0,0 +1,15 @@
+import { IImages } from "../images";
+
+import architecture from "../../public/assets/university/architecture.webp";
+import bank from "../../public/assets/university/bank.webp";
+import blackmarket from "../../public/assets/university/blackmarket.webp";
+import pimp from "../../public/assets/university/pimp.webp";
+
+const universityImages: IImages = {
+  architecture,
+  bank,
+  blackmarket,
+  pimp,
+};
+
+export default universityImages;
