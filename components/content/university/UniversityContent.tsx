df69130ff363214e93ab6f10e2e1f700cc371139
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-08-11 16:40:36 +0200

    feat: add university content component

---
+++ components/content/university/UniversityContent.tsx
@@ -0,0 +1,7 @@
+import styles from "./UniversityContent.module.scss";
+
+const UniversityContent: React.FC = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default UniversityContent;
