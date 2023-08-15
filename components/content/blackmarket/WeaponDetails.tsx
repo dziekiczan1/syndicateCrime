04eec8be38f91b508800d030e9e9756804675ea9
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-08-15 12:11:49 +0200

    feat: add Gang Action compontent and styles

--- components/content/blackmarket/WeaponDetails.tsx
+++ components/content/blackmarket/WeaponDetails.tsx
@@ -1,11 +1,12 @@
 import Button from "@/components/ui/button/Button";
 import { IBlackMarketActions } from "@/constants/actions/blackmarketactions";
 import { formatNumber } from "@/lib/money";
+import { Weapon } from "@/pages/api/user/blackmarketActions";
 import { useEffect, useState } from "react";
 
 interface IWeaponDetails {
   weapon: IBlackMarketActions;
-  handleWeaponAction: (weapon: any, action: string) => void;
+  handleWeaponAction: (weapon: Weapon, action: string) => void;
 }
 
 const WeaponDetails: React.FC<IWeaponDetails> = ({
--- components/content/gang/GangContent.tsx
+++ components/content/gang/GangContent.tsx
@@ -1,13 +1,45 @@
 import PageHeader from "@/components/ui/pageheader/PageHeader";
+import ResponseHandler from "@/components/ui/responsehandler/ResponseHandler";
+import { gangActions } from "@/constants/actions/gangactions";
 import pageDescriptions from "@/constants/descriptions/pagedescriptions";
+import useResponseHandler from "@/lib/useResponseHandler";
+import UserContext from "@/store/user-context";
+import { useContext, useRef } from "react";
+import GangAction from "./GangAction";
 import styles from "./GangContent.module.scss";
 
 const GangContent: React.FC = () => {
   const pageData = pageDescriptions.gang;
+  const { user } = useContext(UserContext);
+  const messageRef = useRef<HTMLDivElement>(null);
+
+  const { errorMessage, actionMessage, isLoading, handleAction } =
+    useResponseHandler(messageRef);
+
+  const handleGangAction = async (gang: any) => {
+    await handleAction("/api/user/gangActions", { gang });
+  };
 
   return (
     <div className={styles.container}>
       <PageHeader pageData={pageData} />
+      <ResponseHandler
+        isLoading={isLoading}
+        errorMessage={errorMessage}
+        actionMessage={actionMessage}
+        messageRef={messageRef}
+      />
+      <p className={styles.tableHeading}>Select a gang to stand with:</p>
+      <div className={styles.actionsContainer}>
+        {user &&
+          gangActions.map((gang, key) => (
+            <GangAction
+              key={key}
+              gang={gang}
+              handleGangAction={handleGangAction}
+            />
+          ))}
+      </div>
     </div>
   );
 };
