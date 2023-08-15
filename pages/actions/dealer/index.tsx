8da4ca57b639dbe58ce50af37147d6fd728c429e
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-08-15 10:02:58 +0200

    refactor: update actions page with in prison check

--- pages/actions/dealer/index.tsx
+++ pages/actions/dealer/index.tsx
@@ -1,7 +1,7 @@
 import DealerContent from "@/components/content/dealer/DealerContent";
 import GameLayout from "@/components/layout/game/GameLayout";
-import { GetServerSidePropsContext } from "next";
-import { getSession } from "next-auth/react";
+import { withPrisonCheck } from "@/lib/withPrisonCheck";
+import { withSessionCheck } from "@/lib/withSessionCheck";
 
 export default function DealerScreen() {
   return (
@@ -11,19 +11,10 @@ export default function DealerScreen() {
   );
 }
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const session = await getSession(context);
-
-  if (!session) {
+export const getServerSideProps = withPrisonCheck(
+  withSessionCheck(async () => {
     return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
+      props: {},
     };
-  }
-
-  return {
-    props: {},
-  };
-}
+  })
+);
