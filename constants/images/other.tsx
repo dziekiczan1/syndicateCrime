11852252962d6bed748e76b5d35496ad5c54a998
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-09-05 19:02:02 +0200

    feat: add notfound image for 404 page

---
+++ constants/images/other.tsx
@@ -0,0 +1,9 @@
+import { IImages } from "../images";
+
+import notfound from "../../public/assets/other/notfound.webp";
+
+const otherImages: IImages = {
+  notfound,
+};
+
+export default otherImages;
