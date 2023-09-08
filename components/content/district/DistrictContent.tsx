f1166ee16aee7386e49ba015b4d8777de1baa931
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-09-08 20:14:47 +0200

    feat: add district component

---
+++ components/content/district/DistrictContent.tsx
@@ -0,0 +1,15 @@
+import PageHeader from "@/components/ui/pageheader/PageHeader";
+import pageDescriptions from "@/constants/descriptions/pagedescriptions";
+import styles from "./DistrictContent.module.scss";
+
+const DistrictContent = () => {
+  const pageData = pageDescriptions.alley;
+
+  return (
+    <div className={styles.container}>
+      <PageHeader pageData={pageData} />
+    </div>
+  );
+};
+
+export default DistrictContent;
