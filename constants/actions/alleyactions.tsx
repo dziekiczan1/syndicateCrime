57f741f6fb8a6d444a071099963d43f8e6c7cf0e
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-09-09 22:11:34 +0200

    feat: add all alley missions

--- constants/actions/alleyactions.tsx
+++ constants/actions/alleyactions.tsx
@@ -6,6 +6,10 @@ export interface IAlleyActions {
 }
 
 export const alleyActions = (userStats: any): IAlleyActions[] => {
+  const userUniversityLength = Object.keys(userStats.university).filter(
+    (key) => userStats.university[key] === true
+  ).length;
+
   return [
     {
       name: "Bank Heist",
@@ -20,6 +24,34 @@ export const alleyActions = (userStats: any): IAlleyActions[] => {
         "The underworld's top fugitive requires your help. Assist in engineering daring prison breaks and help them escape from the clutches of the law not once, but ten times.",
       userValue: userStats?.prison?.escapes || 0,
       requiredValue: 50,
+    },
+    {
+      name: "Master of Sabotage",
+      description:
+        "Become the master of chaos by sabotaging the plans of your rivals. Your target: at least 100 individuals. The more chaos, the better your reputation in the criminal world.",
+      userValue: userStats?.sabotage.totalSabotages || 0,
+      requiredValue: 100,
+    },
+    {
+      name: "Academic Excellence",
+      description:
+        "Show your intellectual prowess by completing all university courses. Knowledge is power, and in the world of syndicates, being well-educated can open new doors.",
+      userValue: userUniversityLength || 0,
+      requiredValue: 4,
+    },
+    {
+      name: "Respect is Earned",
+      description:
+        "Respect is the currency of the streets. Prove your worth by earning 5,000 respect points and solidify your status as a true gangster.",
+      userValue: userStats?.defaultParams.respect || 0,
+      requiredValue: 5000,
+    },
+    {
+      name: "Intellectual Savvy",
+      description:
+        "Intelligence is your greatest weapon. Accumulate 5,000 intelligence points through cunning strategies and astute decision-making to dominate the criminal underworld.",
+      userValue: userStats?.defaultParams.intelligence || 0,
+      requiredValue: 5000,
     },
   ];
 };
