f424ae00e09d6737a2a962ffdfdc3a8493699017
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-09-10 13:49:20 +0200

    feat: add ui to alley actions

--- components/content/alley/AlleyAction.tsx
+++ components/content/alley/AlleyAction.tsx
@@ -5,10 +5,15 @@ import styles from "./AlleyAction.module.scss";
 
 interface IAlleyDetails {
   mission: IAlleyActions;
-  handleAlleyAction: (mission: any) => void;
+  handleAlleyAction: (mission: IAlleyActions) => void;
 }
 
 const AlleyAction = ({ mission, handleAlleyAction }: IAlleyDetails) => {
+  const isMissionCompleted = (
+    userValue: string | number,
+    requiredValue: string | number
+  ) => parseInt(String(userValue)) >= parseInt(String(requiredValue));
+
   return (
     <div className={styles.actionsContent}>
       <div className={styles.actionImage}>
@@ -24,16 +29,38 @@ const AlleyAction = ({ mission, handleAlleyAction }: IAlleyDetails) => {
         <div className={styles.actionDetails}>
           <p className={styles.actionDescription}>{mission.description}</p>
           <div className={styles.actionCosts}>
+            <p className="custom-label">Requirements:</p>
+            <p
+              className={`${styles.actionCost} ${
+                isMissionCompleted(mission.userValue, mission.requiredValue)
+                  ? styles.success
+                  : styles.failed
+              }`}
+            >
+              <span className={styles.costName}>Your value: </span>
+              {mission.userValue}
+            </p>
             <p className={styles.actionCost}>
               <span className={styles.costName}>Required: </span>
               {mission.requiredValue}
             </p>
-            <p className={styles.actionCost}>
-              <span className={styles.costName}>Your value: </span>
-              {mission.userValue}
-            </p>
           </div>
-          <Button onClick={() => handleAlleyAction(mission)} secondary fullSize>
+          <div className={styles.actionCosts}>
+            <p className="custom-label">Bonus:</p>
+            <p className={styles.costName}>+ {mission.bonus.money}</p>
+            <p className={styles.costName}>
+              + {mission.bonus.statValue} {mission.bonus.stat}
+            </p>
+            <p className={styles.costName}>+ {mission.bonus.extra}</p>
+          </div>
+          <Button
+            onClick={() => handleAlleyAction(mission)}
+            secondary
+            fullSize
+            disabled={
+              !isMissionCompleted(mission.userValue, mission.requiredValue)
+            }
+          >
             Collect
           </Button>
         </div>
--- constants/actions/alleyactions.tsx
+++ constants/actions/alleyactions.tsx
@@ -3,11 +3,18 @@ import { StaticImageData } from "next/image";
 import alleyImages from "../images/alley";
 
 export interface IAlleyActions {
+  short: string;
   name: string;
   imageSrc: StaticImageData;
   description: string;
   userValue: number | string;
   requiredValue: number | string;
+  bonus: {
+    money: number | string;
+    statValue: number;
+    stat: string;
+    extra: string;
+  };
 }
 
 export const alleyActions = (userStats: any): IAlleyActions[] => {
@@ -19,52 +26,94 @@ export const alleyActions = (userStats: any): IAlleyActions[] => {
 
   return [
     {
+      short: "heist",
       name: "Bank Heist",
       imageSrc: alleyImages.heist,
       description:
         "A wealthy client needs a million dollars stashed safely in the bank's vault. Are you up for the challenge of pulling off the perfect bank heist and securing the fortune?",
       userValue: formatNumber(userStats?.bank || 0),
       requiredValue: formatNumber(1000000),
+      bonus: {
+        money: formatNumber(100000),
+        statValue: 100,
+        stat: "endurance",
+        extra: "1 market share",
+      },
     },
     {
+      short: "escape",
       name: "Great Escape",
       imageSrc: alleyImages.escape,
       description:
         "The underworld's top fugitive requires your help. Assist in engineering daring prison breaks and help them escape from the clutches of the law not once, but ten times.",
       userValue: userStats?.prison?.escapes || 0,
       requiredValue: 50,
+      bonus: {
+        money: formatNumber(125000),
+        statValue: 250,
+        stat: "charisma",
+        extra: "1 extra weapon",
+      },
     },
     {
+      short: "sabotage",
       name: "Master of Sabotage",
       imageSrc: alleyImages.sabotage,
       description:
         "Become the master of chaos by sabotaging the plans of your rivals. Your target: at least 100 individuals. The more chaos, the better your reputation in the criminal world.",
       userValue: userStats?.sabotage.totalSabotages || 0,
       requiredValue: 100,
+      bonus: {
+        money: formatNumber(150000),
+        statValue: 350,
+        stat: "respect",
+        extra: "1 extra whore",
+      },
     },
     {
+      short: "university",
       name: "Academic Excellence",
       imageSrc: alleyImages.university,
       description:
         "Show your intellectual prowess by completing all university courses. Knowledge is power, and in the world of syndicates, being well-educated can open new doors.",
       userValue: userUniversityLength || 0,
       requiredValue: 4,
+      bonus: {
+        money: formatNumber(200000),
+        statValue: 500,
+        stat: "intelligence",
+        extra: "2 market shares",
+      },
     },
     {
+      short: "respect",
       name: "Respect is Earned",
       imageSrc: alleyImages.respect,
       description:
         "Respect is the currency of the streets. Prove your worth by earning 5,000 respect points and solidify your status as a true gangster.",
       userValue: userStats?.defaultParams.respect || 0,
-      requiredValue: 5000,
+      requiredValue: 10000,
+      bonus: {
+        money: formatNumber(250000),
+        statValue: 1000,
+        stat: "respect",
+        extra: "2 extra weapons",
+      },
     },
     {
+      short: "intelligence",
       name: "Intellectual Savvy",
       imageSrc: alleyImages.intelligence,
       description:
         "Accumulate 5,000 intelligence points through cunning strategies and astute decision-making to dominate the criminal underworld.",
       userValue: userStats?.defaultParams.intelligence || 0,
-      requiredValue: 5000,
+      requiredValue: 10000,
+      bonus: {
+        money: formatNumber(250000),
+        statValue: 1000,
+        stat: "intelligence",
+        extra: "2 extra whores",
+      },
     },
   ];
 };
