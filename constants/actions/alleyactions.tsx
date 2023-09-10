f3e918c553985df576b7d5677441955a31d0d1b5
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-09-10 10:54:59 +0200

    feat: add alley images

--- constants/actions/alleyactions.tsx
+++ constants/actions/alleyactions.tsx
@@ -1,5 +1,9 @@
+import { StaticImageData } from "next/image";
+import alleyImages from "../images/alley";
+
 export interface IAlleyActions {
   name: string;
+  imageSrc: StaticImageData;
   description: string;
   userValue: number;
   requiredValue: number;
@@ -13,6 +17,7 @@ export const alleyActions = (userStats: any): IAlleyActions[] => {
   return [
     {
       name: "Bank Heist",
+      imageSrc: alleyImages.heist,
       description:
         "A wealthy client needs a million dollars stashed safely in the bank's vault. Are you up for the challenge of pulling off the perfect bank heist and securing the fortune?",
       userValue: userStats?.bank || 0,
@@ -20,6 +25,7 @@ export const alleyActions = (userStats: any): IAlleyActions[] => {
     },
     {
       name: "Great Escape",
+      imageSrc: alleyImages.escape,
       description:
         "The underworld's top fugitive requires your help. Assist in engineering daring prison breaks and help them escape from the clutches of the law not once, but ten times.",
       userValue: userStats?.prison?.escapes || 0,
@@ -27,6 +33,7 @@ export const alleyActions = (userStats: any): IAlleyActions[] => {
     },
     {
       name: "Master of Sabotage",
+      imageSrc: alleyImages.sabotage,
       description:
         "Become the master of chaos by sabotaging the plans of your rivals. Your target: at least 100 individuals. The more chaos, the better your reputation in the criminal world.",
       userValue: userStats?.sabotage.totalSabotages || 0,
@@ -34,6 +41,7 @@ export const alleyActions = (userStats: any): IAlleyActions[] => {
     },
     {
       name: "Academic Excellence",
+      imageSrc: alleyImages.university,
       description:
         "Show your intellectual prowess by completing all university courses. Knowledge is power, and in the world of syndicates, being well-educated can open new doors.",
       userValue: userUniversityLength || 0,
@@ -41,6 +49,7 @@ export const alleyActions = (userStats: any): IAlleyActions[] => {
     },
     {
       name: "Respect is Earned",
+      imageSrc: alleyImages.respect,
       description:
         "Respect is the currency of the streets. Prove your worth by earning 5,000 respect points and solidify your status as a true gangster.",
       userValue: userStats?.defaultParams.respect || 0,
@@ -48,6 +57,7 @@ export const alleyActions = (userStats: any): IAlleyActions[] => {
     },
     {
       name: "Intellectual Savvy",
+      imageSrc: alleyImages.intelligence,
       description:
         "Intelligence is your greatest weapon. Accumulate 5,000 intelligence points through cunning strategies and astute decision-making to dominate the criminal underworld.",
       userValue: userStats?.defaultParams.intelligence || 0,
