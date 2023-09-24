c383638ba2d40a33a00a660724933edeecdfb1bb
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-09-24 11:04:40 +0200

    refactor: refactor formatTime function to handel correct display

--- lib/missionTime.tsx
+++ lib/missionTime.tsx
@@ -2,14 +2,10 @@ export function formatTime(seconds: number) {
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
 
-  const formattedHours = hours > 0 ? hours.toString() : "";
+  const formattedHours = hours > 0 ? hours.toString().padStart(2, "0") : "00";
   const formattedMinutes = minutes.toString().padStart(2, "0");
 
-  if (formattedHours) {
-    return `${formattedHours}:${formattedMinutes}`;
-  } else {
-    return `${formattedMinutes}`;
-  }
+  return `${formattedHours}:${formattedMinutes}`;
 }
 
 export function calculatePercentage(number1: number, number2: number) {
@@ -17,10 +13,10 @@ export function calculatePercentage(number1: number, number2: number) {
     return 0;
   }
 
-  const timePercentage = Math.round((number2 / number1) * 100);
+  const timePercentage = (number2 / number1) * 100;
   const maxPercentage = 100;
 
-  const percentage = maxPercentage - timePercentage;
+  const percentage = Math.floor(maxPercentage - timePercentage);
 
   return percentage;
 }
