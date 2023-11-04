707df1ad536ddc2cfe4d8c5fdf856e37d28e91d2
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-11-04 19:40:21 +0100

    feat: add more guide images

--- constants/images/guide.tsx
+++ constants/images/guide.tsx
@@ -1,11 +1,21 @@
 import { IImages } from "../images";
 
+import userbank from "../../public/assets/guide/userbank.webp";
+import usermenu from "../../public/assets/guide/usermenu.webp";
 import userrobbery from "../../public/assets/guide/userrobbery.webp";
+import userrobberyone from "../../public/assets/guide/userrobberyone.webp";
 import userstats from "../../public/assets/guide/userstats.webp";
+import userwhores from "../../public/assets/guide/userwhores.webp";
+import userwhoresone from "../../public/assets/guide/userwhoresone.webp";
 
 const guideImages: IImages = {
   userrobbery,
+  userrobberyone,
   userstats,
+  userwhores,
+  userwhoresone,
+  usermenu,
+  userbank,
 };
 
 export default guideImages;
